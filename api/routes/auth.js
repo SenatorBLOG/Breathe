0b3f6b4cc7e1a4ac87a0b26ca162caf2d5353f83
SenatorBLOG <87308663+SenatorBLOG@users.noreply.github.com>
2026-03-03 19:54:30 -0800

    v1.3.5

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -58,14 +58,6 @@ router.post('/login', async (req, res) => {
   }
 });
 
-router.options('/google', (req, res) => {
-  const origin = req.headers.origin || '';
-  res.setHeader('Access-Control-Allow-Origin', origin);
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  return res.sendStatus(204);
-});
 
 // Google Auth
 router.post('/google', async (req, res) => {
