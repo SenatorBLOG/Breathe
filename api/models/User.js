bbb6c751234be9b2f9d314bf2ac5c0c75f6db618
SenatorBLOG <87308663+SenatorBLOG@users.noreply.github.com>
2026-03-04 23:44:36 -0800

    Update User.js

--- api/models/User.js
+++ api/models/User.js
@@ -24,6 +24,7 @@ const userSchema = new mongoose.Schema(
       enum: ['password', 'google'],
       default: 'password'
     },
+    googleId: { type: String, unique: true, sparse: true },
   },
   { timestamps: true }
 );
