d240ed0f13b735121b773a5cf1d02bff88e97410
SenatorBLOG <87308663+SenatorBLOG@users.noreply.github.com>
2026-03-05 20:08:53 -0800

    Update User.js

--- api/models/User.js
+++ api/models/User.js
@@ -19,6 +19,8 @@ const userSchema = new mongoose.Schema(
       },
       minlength: 6,
     },
+    name: { type: String },     // <--- Add this
+    picture: { type: String },
     authType: {
       type: String,
       enum: ['password', 'google'],
