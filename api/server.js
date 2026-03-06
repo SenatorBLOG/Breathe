17426f1e68a52a0d0531c554d31887c5786c07eb
SenatorBLOG <87308663+SenatorBLOG@users.noreply.github.com>
2026-03-05 20:43:08 -0800

    v2.1.4

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const rateLimit = require('express-rate-limit');
 
 dotenv.config();
 
-const app = require("./app");
+const app = express();
 
 const allowedOrigins = [
   'https://breatheonline.app',
@@ -86,4 +86,4 @@ if (process.env.NODE_ENV !== 'production') {
   app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 }
 
-module.exports = serverless(app);
+module.exports = require('serverless-http')(app);
