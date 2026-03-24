b3aa7e20b3b61ac7d6daf0da19c4a4eac73e676a
SenatorBLOG <87308663+SenatorBLOG@users.noreply.github.com>
2026-03-24 10:21:02 -0700

    v3.7.14

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 // src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import App from './App';
 import './i18n';
 import './theme.css'; // must be imported before App
